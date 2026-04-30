de002ac31b5a05db63a5f4cdafe70c34de940c58
Der-Feb <bugingoderfeb@gmail.com>
2026-04-30 20:49:58 +0200

    feat: improving the ai movement script

--- Assets/Scripts/AI_Movement.cs
+++ Assets/Scripts/AI_Movement.cs
@@ -1,72 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
+ 
 public class AI_Movement : MonoBehaviour
 {
+ 
     Animator animator;
-
+ 
     public float moveSpeed = 0.2f;
-
+ 
+    Vector3 stopPosition;
+ 
     float walkTime;
     public float walkCounter;
     float waitTime;
     public float waitCounter;
-
-    int walkDirection;
-
+ 
+    int WalkDirection;
+ 
     public bool isWalking;
-
-    Vector3 stopPosition;
-
+ 
+    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-
+ 
         //So that all the prefabs don't move/stop at the same time
         walkTime = Random.Range(3,6);
         waitTime = Random.Range(5,7);
-
+ 
+ 
         waitCounter = waitTime;
         walkCounter = walkTime;
  
         ChooseDirection();
     }
-
-    public void ChooseDirection()
-    {
-        walkDirection = Random.Range(0, 4);
-
-        isWalking = true;
-        walkCounter = walkTime;
-    }
-
+ 
+    // Update is called once per frame
     void Update()
     {
-        animator.SetBool("isRunning", true);
-        walkCounter -= Time.deltaTime;
-
-        // avoiding using switch cases
-        float[] directions = {0f, 90f, -90f, 180f};
-
-        transform.localRotation = Quaternion.Euler(0f, directions[walkDirection], 0f);
-        transform.position += transform.forward * moveSpeed * Time.deltaTime;
-
-        if(walkCounter >= 0)
+        if (isWalking)
         {
-            stopPosition = transform.position;
-            isWalking = false;
+ 
+            animator.SetBool("isRunning", true);
+            walkCounter -= Time.deltaTime;
 
-            // stop walking
-            transform.position = stopPosition;
-            animator.SetBool("isRunning", false);
-
-            //reset the waitCounter
-            waitCounter = waitTime;
+            float[] rotationDirections = {0f, 90f, -90f, 180f};
+ 
+            transform.localRotation = Quaternion.Euler(0f, rotationDirections[WalkDirection], 0f);
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+ 
+            if (walkCounter <= 0)
+            {
+                stopPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                isWalking = false;
+                //stop movement
+                transform.position = stopPosition;
+                animator.SetBool("isRunning", false);
+                //reset the waitCounter
+                waitCounter = waitTime;
+            }
         }
         else
         {
-            walkCounter -= Time.deltaTime;
-            if (waitCounter <= 0) ChooseDirection();
+ 
+            waitCounter -= Time.deltaTime;
+ 
+            if (waitCounter <= 0)
+            {
+                ChooseDirection();
+            }
         }
     }
-
+ 
+ 
+    public void ChooseDirection()
+    {
+        WalkDirection = Random.Range(0, 4);
+ 
+        isWalking = true;
+        walkCounter = walkTime;
+    }
 }
